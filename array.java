63119a08b0a95432827f08ca792cad430463b1fa
Dharun prasanth <s.dharun7507@gmail.com>
2026-07-20 14:35:18 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,8 @@
+public class main{
+    public static void main(String[]args){
+        int[]arr={10,20,30,40,50};
+        System.out.println(arr[0]);
+        System.out.println(arr[3]);
+    }
+}
+       
