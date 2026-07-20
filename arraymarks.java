19e5389aa6df7b120643e99999f9de6cd8668a38
Dharun prasanth <s.dharun7507@gmail.com>
2026-07-20 14:48:34 +0530

    Create arraymarks.java

---
+++ arraymarks.java
@@ -0,0 +1,10 @@
+public class main{
+    public static void main(String[]args){
+        int[]mark={70,90,80,60,50};
+        String[] subject={"Tamil","English","Maths","Science","Social"};
+        for(int i=0;i<mark.length;i++)
+        {
+            System.out.println(subject[i]+"="+mark[i]);
+        }
+    }
+}
